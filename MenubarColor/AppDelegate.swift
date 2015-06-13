d5015e4a83e2f1685afb83fa5b6eecc2b67bbb27
Kensuke Hoshikawa <kensuke1751@gmail.com>
2015-06-13 11:41:09 +0900

    Outside Windows click, close app.

--- MenubarColor/AppDelegate.swift
+++ MenubarColor/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
     let popover = NSPopover()
+    var eventMonitor: EventMonitor?
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
@@ -22,16 +23,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             button.action = Selector("togglePopover:")
         }
         popover.contentViewController = MenubarColorViewController(nibName: "MenubarColorViewController", bundle: nil)
+        eventMonitor = EventMonitor(mask: .LeftMouseDownMask | .RightMouseDownMask) { [unowned self] event in
+            if self.popover.shown {
+                self.closePopover(event)
+            }
+        }
+        eventMonitor?.start()
     }
 
     func showPopover(sender: AnyObject?) {
         if let button = statusItem.button {
             popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSMinYEdge)
         }
+        eventMonitor?.start()
     }
 
     func closePopover(sender: AnyObject?) {
         popover.performClose(sender)
+        eventMonitor?.stop()
     }
 
     func togglePopover(sender: AnyObject?) {
---
+++ MenubarColor/EventMonitor.swift
@@ -0,0 +1,35 @@
+//
+//  EventMonitor.swift
+//  MenubarColor
+//
+//  Created by Kensuke Hoshikawa on 2015/06/13.
+//  Copyright (c) 2015年 star__hoshi. All rights reserved.
+//
+
+import Cocoa
+
+public class EventMonitor {
+    private var monitor: AnyObject?
+    private let mask: NSEventMask
+    private let handler: NSEvent? -> ()
+
+    public init(mask: NSEventMask, handler: NSEvent? -> ()) {
+        self.mask = mask
+        self.handler = handler
+    }
+
+    deinit {
+        stop()
+    }
+
+    public func start() {
+        monitor = NSEvent.addGlobalMonitorForEventsMatchingMask(mask, handler: handler)
+    }
+
+    public func stop() {
+        if monitor != nil {
+            NSEvent.removeMonitor(monitor!)
+            monitor = nil
+        }
+    }
+}
