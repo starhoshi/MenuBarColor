2344b886658edc3b8c67336f3adfa53ca2128a62
Kensuke Hoshikawa <kensuke1751@gmail.com>
2015-06-13 10:43:55 +0900

    Show MenuIcon, Hide Dock.

--- MenubarColor/AppDelegate.swift
+++ MenubarColor/AppDelegate.swift
@@ -12,14 +12,25 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
 
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = Selector("printMenubarColor:")
+        }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+    }
+
+    func printMenubarColor(sender: AnyObject) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+
+        println("\(quoteText) — \(quoteAuthor)")
     }
 
 
