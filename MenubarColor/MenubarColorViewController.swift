379395b6145539de049744f3d915e830fcc089e1
Kensuke Hoshikawa <kensuke1751@gmail.com>
2015-06-13 11:31:18 +0900

    Implement View Controller, View Components.

--- MenubarColor/MenubarColorViewController.swift
+++ MenubarColor/MenubarColorViewController.swift
@@ -9,10 +9,37 @@
 import Cocoa
 
 class MenubarColorViewController: NSViewController {
+    @IBOutlet var textLabel: NSTextField!
+    let menubarColors = MenubarColor.all
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do view setup here.
+    var currentMenubarColorIndex: Int = 0 {
+        didSet {
+            updateMenubarColor()
+        }
     }
-    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+
+        currentMenubarColorIndex = 0
+    }
+
+    func updateMenubarColor() {
+        textLabel.stringValue = toString(menubarColors[currentMenubarColorIndex])
+    }
+}
+
+// MARK: Actions
+
+extension MenubarColorViewController {
+    @IBAction func goLeft(sender: NSButton) {
+        currentMenubarColorIndex = (currentMenubarColorIndex - 1 + menubarColors.count) % menubarColors.count
+    }
+
+    @IBAction func goRight(sender: NSButton) {
+        currentMenubarColorIndex = (currentMenubarColorIndex + 1) % menubarColors.count
+    }
+
+    @IBAction func quit(sender: NSButton) {
+        NSApplication.sharedApplication().terminate(sender)
+    }
 }
