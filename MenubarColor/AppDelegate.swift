dfbed763f6411d57a54e45210f0335eb4d12e084
Kensuke Hoshikawa <kensuke1751@gmail.com>
2015-06-13 10:39:36 +0900

    initial commit.

---
+++ MenubarColor/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MenubarColor
+//
+//  Created by Kensuke Hoshikawa on 2015/06/13.
+//  Copyright (c) 2015年 star__hoshi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
